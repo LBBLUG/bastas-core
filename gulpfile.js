51102926a7975d28aa46aee1055464b9bb43d4f8
Dan Ferguson <daniefer@icloud.com>
2016-07-16 13:51:21 -0500

    Moved devDependencies to the correct place
    moved dependencies around to match best practice. Updated Gulpfile so that it also downloads typings

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var gulpTypings = require("gulp-typings");
+
+
+gulp.task("installTypings",function(){
+    var stream = gulp.src("./typings.json")
+        .pipe(gulpTypings()); //will install all typingsfiles in pipeline.
+    return stream; // by returning stream gulp can listen to events from the stream and knows when it is finished.
+});
+
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task("default", function () {
+gulp.task("default", ["installTypings"], function () {
     var tsResult = tsProject.src()
         .pipe(ts(tsProject));
     return tsResult.js.pipe(gulp.dest("lib"));
